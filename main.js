bb0d6274fe3dd450eecf896202e0a6129d7e055e
Jadslog <jadslog@hotmail.com>
2024-06-21 21:33:02 -0600

    Input Validation JavaScript

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const calculateBtn = document.querySelector(".bmr-calculator .result .calculate-
 const age = document.querySelector(".bmr-calculator form #age");
 const height = document.querySelector(".bmr-calculator form #height");
 const weight = document.querySelector(".bmr-calculator form #weight");
+const errorMessage = document.querySelector(".bmr-calculator .result .error-message");
 
 //Male
 //BMR = 10*weight + 6.25*height - 5*age + 5
@@ -17,9 +18,49 @@ const calculateBMR = (weight, height, age, gender) => {
 }
 
 calculateBtn.addEventListener("click", () => {
+
+    if (age.classList.contains("invalid") || height.classList.contains("invalid") || weight.classList.contains("invalid")) {
+        errorMessage.classList.add("active");
+        return;
+    }
+    
+    errorMessage.classList.remove("active");
+    
     let genderValue = document.querySelector(".bmr-calculator form input[name='gender']:checked").value;
         
     let BMR = calculateBMR(weight.value, height.value, age.value, genderValue);
 
-    calories.innerHTML = BMR.toLocaleString("en_US");
+    calories.innerHTML = BMR.toLocaleString("en-US");
+});
+
+// Input Validation
+
+age.addEventListener("input", (e) => {
+    let ageValue = e.target.value;
+
+    if (!ageValue || isNaN(ageValue) || ageValue < 10 || ageValue > 100) {
+        age.classList.add("invalid");
+    } else {
+        age.classList.remove("invalid");
+    }
+});
+
+height.addEventListener("input", (e) => {
+    let heightValue = e.target.value;
+
+    if (!heightValue || isNaN(heightValue) || heightValue < 0) {
+        height.classList.add("invalid");
+    } else {
+        height.classList.remove("invalid");
+    }
+});
+
+weight.addEventListener("input", (e) => {
+    let weightValue = e.target.value;
+
+    if (!weightValue || isNaN(weightValue) || weightValue < 0) {
+        weight.classList.add("invalid");
+    } else {
+        weight.classList.remove("invalid");
+    }
 });
